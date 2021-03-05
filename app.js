5e086e6576d00d3cf783d653da3b3fb1a32aeff5
Supman001 <supmansullivan001@gmail.com>
2021-03-05 17:29:55 +0100

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const staffList = [];
+const staffList = [];
 
 let staffMembers = [{id: 1, role: "Engineer", name: "Ifeco", countDays: 1},
 {id: 2, role: "Director", name: "Kingsley", countDays: 0},
@@ -21,7 +21,7 @@ function boardMembers(){
                 date:date
             }
             
-            staffMembers.push(newStaffData)
+            staffList.push(newStaffData)
             console.log(staffMembers[p])  
             return;
 
@@ -30,3 +30,20 @@ function boardMembers(){
         }
 }
     
+
+function payStaff(){
+    let staffID = document.getElementById('staffIds').value
+
+    let days = 0
+    for(let tdays = 0; tdays < staffList.length ; tdays++){
+        if(staffList[tdays].id == staffID){
+            days +=1
+        }
+    }
+    if(days >= 5){
+        alert('You are eligible to receive your salary')
+    }else{
+        alert('You are not qualified to be payed')
+    }
+
+}
